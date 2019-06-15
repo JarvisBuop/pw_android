c47280274a2859e2194423fb5ea28bc462d46747
jarvisdong <mrjarvisdong@163.com>
2019-06-15 16:07:40 +0800

    all

--- module_welcome/src/main/java/com/jdev/module_welcome/WelcomeActivity.kt
+++ module_welcome/src/main/java/com/jdev/module_welcome/WelcomeActivity.kt
@@ -1,30 +1,30 @@
-package com.jdev.module_welcome
-
-import android.os.Bundle
-import com.airbnb.lottie.LottieAnimationView
-import com.jarvisdong.kotlindemo.ui.BaseActivity
-
-/**
- * Created by JarvisDong on 2019/3/29.
- * OverView:
- */
-
-class WelcomeActivity : BaseActivity() {
-    private lateinit var mAnimationView: LottieAnimationView
-
-
-    override fun initIntentData(): Boolean {
-        return true
-    }
-
-    override fun customOperate(savedInstanceState: Bundle?) {
-        mAnimationView = findViewById<LottieAnimationView>(R.id.animation_view)
-
-        mAnimationView.setAnimation(R.raw.logosmall)
-    }
-
-    override fun getViewStubId(): Int {
-        return R.layout.act_welcome_main
-    }
-
-}
+//package com.jdev.module_welcome
+//
+//import android.os.Bundle
+//import com.airbnb.lottie.LottieAnimationView
+//import com.jarvisdong.kotlindemo.ui.BaseActivity
+//
+///**
+// * Created by JarvisDong on 2019/3/29.
+// * OverView:
+// */
+//
+//class WelcomeActivity : BaseActivity() {
+//    private lateinit var mAnimationView: LottieAnimationView
+//
+//
+//    override fun initIntentData(): Boolean {
+//        return true
+//    }
+//
+//    override fun customOperate(savedInstanceState: Bundle?) {
+//        mAnimationView = findViewById<LottieAnimationView>(R.id.animation_view)
+//
+//        mAnimationView.setAnimation(R.raw.logosmall)
+//    }
+//
+//    override fun getViewStubId(): Int {
+//        return R.layout.act_welcome_main
+//    }
+//
+//}
